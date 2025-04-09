c48e74a2fdab1a5468b3040d70e5a97af1e47321
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-04-09 19:34:58 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function showNextPage(currentPageId, nextPageId) {
+  const currentPage = document.getElementById(currentPageId);
+  const nextPage = document.getElementById(nextPageId);
+
+  // Apply fade-out effect to the current page
+  currentPage.classList.add("fade-out");
+
+  // After fade-out animation ends, hide the current page and show the next page
+  currentPage.addEventListener("animationend", function () {
+    currentPage.style.display = "none"; // Hide current page
+    nextPage.style.display = "block"; // Show next page
+    nextPage.classList.add("fade-in"); // Apply fade-in effect to the next page
+  });
+}
+
+var end = new Date("7/6/2025 10:1 AM");
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+  var now = new Date();
+  var distance = end - now;
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("countdown").innerHTML = "EXPIRED!";
+
+    return;
+  }
+  var days = Math.floor(distance / _day);
+  var hours = Math.floor((distance % _day) / _hour);
+  var minutes = Math.floor((distance % _hour) / _minute);
+  var seconds = Math.floor((distance % _minute) / _second);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+timer = setInterval(showRemaining, 1000);
